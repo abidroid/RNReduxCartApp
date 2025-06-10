95875982fb8b331d69937d4fb30c519c5cfbe138
Muhammad Abid <abidroid1@gmail.com>
2025-06-10 22:25:11 +0500

    screens

---
+++ src/screens/ProductListScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const ProductListScreen = () => {
+
+    return (
+        <View>
+            <Text>ProductList</Text>
+        </View>
+    );
+};
+
+export default ProductListScreen;
